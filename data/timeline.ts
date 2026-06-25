25e54c584a72f6af1d75480e9dabb580e50126e2
Xxxhite <1277229128@qq.com>
2026-06-26 05:22:07 +0800

    chore: test commit

--- data/timeline.ts
+++ data/timeline.ts
@@ -22,14 +22,25 @@ export interface TimelineItem {
 
 export const timelineData: TimelineItem[] = [
     {
-        id: "edu-shenzhen-senior-high",
-        title: "深圳高级中学",
+        id: "edu-senior-high",
+        title: "高中阶段",
         description: "是一段难忘的时光...",
         type: "education",
         startDate: "2020-09-01",
         endDate: "2023-06-11",
         location: "深圳",
         organization: "深圳高级中学",
+        icon: "material-symbols:school",
+        color: "#1890ff",
+    },
+    {
+        id: "edu-collage",
+        title: "大学阶段",
+        description: "努力中",
+        type: "education",
+        startDate: "2023-09-01",
+        location: "杭州",
+        organization: "浙江理工大学",
         icon: "material-symbols:school",
         color: "#1890ff",
     }
