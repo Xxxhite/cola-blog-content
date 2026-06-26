f13e062bb6c52da1ade0c6962078bd5da2a8f3a1
Xxxhite <1277229128@qq.com>
2026-06-27 05:25:17 +0800

    feat(timeline): update timeline

--- data/timeline.ts
+++ data/timeline.ts
@@ -43,5 +43,19 @@ export const timelineData: TimelineItem[] = [
         organization: "浙江理工大学",
         icon: "material-symbols:school",
         color: "#1890ff",
+    },
+    {
+        id: "cola-blog",
+        title: "我的个人博客",
+        description: "绝赞完成了，正在努力维护中",
+        type: "project",
+        startDate: "2026-06-27",
+        links: [{
+            name: "一罐可乐",
+            url: "https://blog.sopak.space",
+            type: "website"
+        }],
+        icon: "tdesign:cola-filled",
+        color: "#F5222D"
     }
 ];
